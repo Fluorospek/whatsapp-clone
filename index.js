ed914daaaaf2e97363a8748f2531c958f5540408
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2023-08-03 23:33:11 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+var io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

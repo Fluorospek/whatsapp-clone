7555c007db49e0460294f4609797e93c99e9c478
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2023-08-08 18:13:54 +0530

    Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 var server = http.createServer(app);
 var io = require("socket.io")(server, {
@@ -13,9 +13,20 @@ var io = require("socket.io")(server, {
 
 app.use(cors());
 app.use(express.json());
+var clients = {};
 
 io.on("connection", (socket) => {
   console.log("Connected");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket.id;
+  });
+  socket.on("message", (data) => {
+    console.log(data);
+    let targetID = data.targetID;
+    if (clients[targetID]) clients[targetID].emit("message", data);
+  });
 });
 
 server.listen(port, "0.0.0.0", () => {
